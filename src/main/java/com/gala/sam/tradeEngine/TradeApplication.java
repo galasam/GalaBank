7e046b91b0338ee12d73c5aad9cda17adf5a7e95
srussell <samuel.russell@galatea-associates.com>
2019-03-19 15:17:05 +0000

    annotated with eureka client

--- src/main/java/com/gala/sam/tradeEngine/TradeApplication.java
+++ src/main/java/com/gala/sam/tradeEngine/TradeApplication.java
@@ -3,9 +3,11 @@ package com.gala.sam.tradeEngine;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @Slf4j
 @SpringBootApplication
+@EnableEurekaClient
 public class TradeApplication {
   public static void main(String[] args) {
     System.setProperty("spring.config.name", "trade-engine");
