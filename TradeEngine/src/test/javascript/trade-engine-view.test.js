c11b47cc66664a63b3f7006c7cbe4a882e4af497
srussell <samue.russell@galatea-associates.com>
2019-04-01 13:40:36 +0100

    added babel convertion to build

--- TradeEngine/src/test/javascript/trade-engine-view.test.js
+++ TradeEngine/src/test/javascript/trade-engine-view.test.js
@@ -2,7 +2,7 @@ var path = require("path")
 var ReactDOM = require('react-dom')
 var act = require('react-dom/test-utils').act
 var engine = path.join(__dirname, "../../main/resources/public/tradeEngine/trade-engine-view.js")
-//var engine = require(engine)
+var engine = require(engine)
 
 it("displays a trade", () => {
   act(() => {})
