a1e5c56b0b9cf60e52333ea2a667245c6ea4bfb2
srussell <samue.russell@galatea-associates.com>
2019-04-02 14:30:13 +0100

    disable source map dev tool

--- TradeEngine/src/main/resources/js/webpack.config.js
+++ TradeEngine/src/main/resources/js/webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   mode: 'development',
-  devtool: "#inline-source-map",
+  /*devtool: "#inline-source-map",*/
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
