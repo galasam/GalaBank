0436b8b3ca8c8db9f0a9388f513f834ed710ba17
srussell <samue.russell@galatea-associates.com>
2019-04-02 14:25:11 +0100

    exclude tests from prod build

--- TradeEngine/src/main/resources/js/webpack.config.js
+++ TradeEngine/src/main/resources/js/webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
       {
         test: /\.js$/,
         include: path.resolve(__dirname, 'src'),
-        exclude: /node_modules/,
+        exclude: /(node_modules|\.test\.js)/,
         use: {
           loader: 'babel-loader',
           options: {
