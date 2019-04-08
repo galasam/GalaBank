de884d5de441079cb7516d72dbc781a47db41f18
srussell <samue.russell@galatea-associates.com>
2019-04-08 13:04:09 +0100

    tidy limit test class

--- TradeEngine/src/test/java/unit/LimitOrderProcessorTests.java
+++ TradeEngine/src/test/java/unit/LimitOrderProcessorTests.java
@@ -9,45 +9,49 @@ import static org.mockito.Mockito.verify;
 
 import com.gala.sam.tradeEngine.domain.datastructures.LimitOrderQueue;
 import com.gala.sam.tradeEngine.domain.datastructures.LimitOrderQueue.SortingMethod;
+import com.gala.sam.tradeEngine.domain.datastructures.MarketState;
+import com.gala.sam.tradeEngine.domain.datastructures.OrderIdPriorityQueue;
+import com.gala.sam.tradeEngine.domain.datastructures.TickerData;
 import com.gala.sam.tradeEngine.domain.enteredorder.LimitOrder;
 import com.gala.sam.tradeEngine.domain.enteredorder.MarketOrder;
-import com.gala.sam.tradeEngine.domain.datastructures.MarketState;
-import com.gala.sam.tradeEngine.domain.datastructures.TickerData;
 import com.gala.sam.tradeEngine.domain.orderrequest.AbstractOrderRequest.Direction;
 import com.gala.sam.tradeEngine.repository.IOrderRepository;
 import com.gala.sam.tradeEngine.repository.ITradeRepository;
 import com.gala.sam.tradeEngine.utils.MarketUtils;
-import com.gala.sam.tradeEngine.utils.exception.OrderDirectionNotSupportedException;
+import com.gala.sam.tradeEngine.utils.exception.AbstractOrderFieldNotSupportedException;
 import com.gala.sam.tradeEngine.utils.exception.OrderTimeInForceNotSupportedException;
 import com.gala.sam.tradeEngine.utils.exception.ProcessingActiveOrderException;
 import com.gala.sam.tradeEngine.utils.orderProcessors.ActiveLimitOrderProcessor;
 import java.util.SortedSet;
-import java.util.TreeSet;
 import org.junit.Test;
 
 public class LimitOrderProcessorTests {
 
   @Test
   public void whenAllQueuesAreEmptyTest()
-      throws OrderTimeInForceNotSupportedException, OrderDirectionNotSupportedException, ProcessingActiveOrderException {
+      throws OrderTimeInForceNotSupportedException, ProcessingActiveOrderException {
     //Given a limit order and empty queues
     LimitOrder limitOrder = LimitOrder.builder().build();
 
-    IOrderRepository orderRepository = RepositoryMockHelper.getEmptyRepository(IOrderRepository.class);
-    ITradeRepository ITradeRepository = RepositoryMockHelper.getEmptyRepository(ITradeRepository.class);
+    IOrderRepository orderRepository = RepositoryMockHelper
+        .getEmptyRepository(IOrderRepository.class);
+    ITradeRepository ITradeRepository = RepositoryMockHelper
+        .getEmptyRepository(ITradeRepository.class);
     MarketState marketState = mock(MarketState.class);
     MarketUtils marketUtils = mock(MarketUtils.class);
 
     ActiveLimitOrderProcessor activeLimitOrderProcessor = new ActiveLimitOrderProcessor(
         orderRepository, ITradeRepository, marketState, marketUtils);
 
-    SortedSet<MarketOrder> marketOrders = new TreeSet<>();
-    SortedSet<LimitOrder> sameTypeLimitOrders = new TreeSet<>();
-    SortedSet<LimitOrder> oppositeTypeLimitOrders = new TreeSet<>();
+    SortedSet<MarketOrder> marketOrders = new OrderIdPriorityQueue<>();
+    SortedSet<LimitOrder> sameTypeLimitOrders = new LimitOrderQueue(SortingMethod.PRICE_ASC);
+    SortedSet<LimitOrder> oppositeTypeLimitOrders = new LimitOrderQueue(SortingMethod.PRICE_ASC);
     TickerData tickerData = mock(TickerData.class);
 
     //When: process is called
-    activeLimitOrderProcessor.processDirectedLimitOrder(limitOrder, tickerData, marketOrders, sameTypeLimitOrders, oppositeTypeLimitOrders);
+    activeLimitOrderProcessor
+        .processDirectedLimitOrder(limitOrder, tickerData, marketOrders, sameTypeLimitOrders,
+            oppositeTypeLimitOrders);
 
     //Then: queueIfTimeInForce is called with the right parameters
     verify(marketUtils).queueIfTimeInForce(eq(limitOrder), eq(sameTypeLimitOrders), any());
@@ -55,28 +59,32 @@ public class LimitOrderProcessorTests {
 
   @Test
   public void whenMarketQueueEmptyNonMatchingLimitOrderInLimitQueue()
-      throws ProcessingActiveOrderException, OrderTimeInForceNotSupportedException, OrderDirectionNotSupportedException {
+      throws ProcessingActiveOrderException, AbstractOrderFieldNotSupportedException {
     //Given: empty market order queue and a non matching limit order in limit order queue
     LimitOrder limitOrder = LimitOrder.builder().direction(Direction.BUY).limit(10.0f).build();
-    LimitOrder nonMatchingLimitOrder = LimitOrder.builder().direction(Direction.SELL).limit(20.0f).build();
+    LimitOrder nonMatchingLimitOrder = LimitOrder.builder().direction(Direction.SELL).limit(20.0f)
+        .build();
 
-    IOrderRepository orderRepository = RepositoryMockHelper.getEmptyRepository(IOrderRepository.class);
-    ITradeRepository ITradeRepository = RepositoryMockHelper.getEmptyRepository(ITradeRepository.class);
+    IOrderRepository orderRepository = RepositoryMockHelper
+        .getEmptyRepository(IOrderRepository.class);
+    ITradeRepository ITradeRepository = RepositoryMockHelper
+        .getEmptyRepository(ITradeRepository.class);
     MarketState marketState = mock(MarketState.class);
     MarketUtils marketUtils = mock(MarketUtils.class);
 
     ActiveLimitOrderProcessor activeLimitOrderProcessor = new ActiveLimitOrderProcessor(
         orderRepository, ITradeRepository, marketState, marketUtils);
 
-    SortedSet<MarketOrder> marketOrders = new TreeSet<>();
-    SortedSet<LimitOrder> sameTypeLimitOrders = new LimitOrderQueue(
-        SortingMethod.PRICE_ASC);
+    SortedSet<MarketOrder> marketOrders = new OrderIdPriorityQueue<>();
+    SortedSet<LimitOrder> sameTypeLimitOrders = new LimitOrderQueue(SortingMethod.PRICE_ASC);
     sameTypeLimitOrders.add(nonMatchingLimitOrder);
-    SortedSet<LimitOrder> oppositeTypeLimitOrders = new TreeSet<>();
+    SortedSet<LimitOrder> oppositeTypeLimitOrders = new LimitOrderQueue(SortingMethod.PRICE_ASC);
     TickerData tickerData = mock(TickerData.class);
 
     //When: process is called
-    activeLimitOrderProcessor.processDirectedLimitOrder(limitOrder, tickerData, marketOrders, sameTypeLimitOrders, oppositeTypeLimitOrders);
+    activeLimitOrderProcessor
+        .processDirectedLimitOrder(limitOrder, tickerData, marketOrders, sameTypeLimitOrders,
+            oppositeTypeLimitOrders);
 
     //Then: no trade is made and queueIfTimeInForce is called with the right parameters
     verify(marketUtils).queueIfTimeInForce(eq(limitOrder), eq(sameTypeLimitOrders), any());
