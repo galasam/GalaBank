0fdc1bffcdcd4f002d9647758b77083b209239bc
srussell <samue.russell@galatea-associates.com>
2019-04-08 16:03:12 +0100

    fixed display bug

--- TradeEngine/src/main/resources/js/src/trade-engine-view.js
+++ TradeEngine/src/main/resources/js/src/trade-engine-view.js
@@ -8,15 +8,14 @@ class Order extends React.Component {
     let order=this.props.order
     console.log(order)
     if (order.limit == null) {
-      var orderTypeName = "Market"
-      var orderTypeClass = "market"
+      return (
+        <li className={"order market"} >#{order.orderId}: {order.clientId} made {order.timeInForce} {orderTypeName} Market Order for {order.quantity} shares of {order.ticker} with {order.quantityRemaining} remaining.</li>
+      );
     } else {
-      var orderTypeName = "Limit"
-      var orderTypeClass = "limit"
+      return (
+        <li className={"order limit"} >#{order.orderId}: {order.clientId} made {order.timeInForce} {orderTypeName} Limit Order for {order.quantity} shares of {order.ticker} at {order.limit} with {order.quantityRemaining} remaining.</li>
+      );
     }
-    return (
-      <li className={"order " + orderTypeClass} >#{order.orderId}: {order.clientId} made {order.timeInForce} {orderTypeName} Order for {order.quantity} shares of {order.ticker} at {order.limit} with {order.quantityRemaining} remaining.</li>
-    );
   }
 }
 
