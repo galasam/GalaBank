bcb05d7ceb193a1c87fee134189c7750e2da9a31
srussell <samue.russell@galatea-associates.com>
2019-04-08 10:21:33 +0100

    rewrite error strings and validators and fix bug

--- TradeEngine/src/main/java/com/gala/sam/tradeEngine/utils/orderValidators/StopMarketOrderValidator.java
+++ TradeEngine/src/main/java/com/gala/sam/tradeEngine/utils/orderValidators/StopMarketOrderValidator.java
@@ -1,6 +1,8 @@
 package com.gala.sam.tradeEngine.utils.orderValidators;
 
 import com.gala.sam.tradeEngine.domain.orderrequest.StopMarketOrderRequest;
+import com.gala.sam.tradeEngine.utils.orderValidators.validatorHelpers.BaseOrderValidatorHelper;
+import com.gala.sam.tradeEngine.utils.orderValidators.validatorHelpers.StopOrderValidatorHelper;
 import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 
@@ -8,9 +10,12 @@ import lombok.extern.slf4j.Slf4j;
 public class StopMarketOrderValidator implements IOrderValidator<StopMarketOrderRequest> {
 
   public List<String> findErrors(StopMarketOrderRequest order) {
-    List<String> errors = BaseOrderValidator.findErrors(order);
-    log.debug("StopLimitOrderValidator has found {} errors on order {} : {}",
-        errors.size(), order, errors);
+    List<String> errors = BaseOrderValidatorHelper.findErrors(order);
+    errors.addAll(StopOrderValidatorHelper.findErrors(order));
+    if (errors.size() > 0) {
+      log.debug("StopLimitOrderValidator has found {} errors on order {} : {}",
+          errors.size(), order, errors);
+    }
     return errors;
   }
 }
--- TradeEngine/src/main/java/com/gala/sam/tradeEngine/utils/orderValidators/validatorHelpers/BaseOrderValidatorHelper.java
+++ TradeEngine/src/main/java/com/gala/sam/tradeEngine/utils/orderValidators/validatorHelpers/BaseOrderValidatorHelper.java
@@ -1,22 +1,23 @@
-package com.gala.sam.tradeEngine.utils.orderValidators;
+package com.gala.sam.tradeEngine.utils.orderValidators.validatorHelpers;
 
 import com.gala.sam.tradeEngine.domain.orderrequest.AbstractOrderRequest;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
-public class BaseOrderValidator {
-   static List<String> findErrors(AbstractOrderRequest order) {
+public class BaseOrderValidatorHelper {
+
+   public static List<String> findErrors(AbstractOrderRequest order) {
       List<String> errors = new ArrayList<>();
       errors.addAll(findErrorsInClientId(order.getClientId()));
       errors.addAll(findErrorsInQuantity(order.getQuantity()));
       return errors;
    }
 
-   static private Collection<String> findErrorsInClientId(int clientId) {
+   static private List<String> findErrorsInClientId(int clientId) {
       List<String> errors = new ArrayList<>();
       if(clientId < 0) {
-         errors.add("clientId price cannot be negative");
+         errors.add("clientId price is negative.");
       }
       return errors;
    }
@@ -24,7 +25,7 @@ public class BaseOrderValidator {
    static private List<String> findErrorsInQuantity(int quantity) {
       List<String> errors = new ArrayList<>();
       if(quantity < 0) {
-         errors.add("quantity price cannot be negative");
+         errors.add("quantity price is negative");
       }
       return errors;
    }
