733d47251ff9b5f089fe2dda63131bd99a7a9e02
srussell <samue.russell@galatea-associates.com>
2019-04-08 16:41:48 +0100

    remove system.out.print

--- TradeEngine/src/main/java/com/gala/sam/tradeEngine/utils/OrderRequestDeserializer.java
+++ TradeEngine/src/main/java/com/gala/sam/tradeEngine/utils/OrderRequestDeserializer.java
@@ -31,7 +31,6 @@ public class OrderRequestDeserializer extends StdDeserializer<AbstractOrderReque
   public AbstractOrderRequest deserialize(JsonParser jsonParser,
       DeserializationContext ctx) throws IOException, JsonProcessingException {
     JsonNode root = jsonParser.getCodec().readTree(jsonParser);
-    System.out.println(root);
     OrderType orderType = OrderType.valueOf(root.get("type").asText());
     int clientId = root.get("clientId").asInt();
     Direction direction = Direction.valueOf(root.get("direction").asText());
