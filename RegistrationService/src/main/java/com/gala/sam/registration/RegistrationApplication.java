dd8883d02146abf06f117d455955b147752af734
srussell <samue.russell@galatea-associates.com>
2019-04-23 14:16:34 +0100

    remove unused args

--- RegistrationService/src/main/java/com/gala/sam/registration/RegistrationApplication.java
+++ RegistrationService/src/main/java/com/gala/sam/registration/RegistrationApplication.java
@@ -10,6 +10,6 @@ public class RegistrationApplication {
 
   public static void main(String[] args) {
     System.setProperty("spring.config.name", "registration-server");
-    SpringApplication.run(RegistrationApplication.class, args);
+    SpringApplication.run(RegistrationApplication.class);
   }
 }
